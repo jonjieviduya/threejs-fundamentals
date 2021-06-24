351d0babc36fd54c75088c92070418f08d99efb0
Jonjie Viduya <jonjieviduya@gmail.com>
2021-06-24 12:55:56 +0800

    Lesson 3.

--- js/main.js
+++ js/main.js
@@ -1,55 +1,87 @@
 import * as THREE from '../threejs/build/three.module.js'
-		
+
 function main() {
     const canvas = document.querySelector('#c')
     const renderer = new THREE.WebGLRenderer({ canvas })
 
-    const fov = 75
+    const fov = 40
     const aspect = 2
     const near = 0.1
-    const far = 5
+    const far = 1000
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
 
-    camera.position.z = 2
+    camera.position.z = 120
 
     const scene = new THREE.Scene()
+    scene.background = new THREE.Color(0xAAAAAA)
 
     {
-        const lightingColor = 0xFFFFFF
+        const color = 0xFFFFFF
         const intensity = 1
-        const light = new THREE.DirectionalLight(lightingColor, intensity)
+        const light = new THREE.DirectionalLight(color, intensity)
         light.position.set(-1, 2, 4)
         scene.add(light)
     }
-
-    const boxWidth = 1
-    const boxHeight = 1
-    const boxDepth = 1
-
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth)
-
-    function makeInstance(geometry, color, x) {
-        const material = new THREE.MeshPhongMaterial({ color: color })
-        const cube = new THREE.Mesh(geometry, material)
-
-        scene.add(cube)
-
-        cube.position.x = x
-
-        return cube
+    {
+        const color = 0xFFFFFF
+        const intensity = 1
+        const light = new THREE.DirectionalLight(color, intensity)
+        light.position.set(1, -2, -4)
+        scene.add(light)
     }
 
-    const cubes = [
-        makeInstance(geometry, 0x44aa88, 0),
-        makeInstance(geometry, 0x8844aa, -2),
-        makeInstance(geometry, 0xaa8844, 2)
-    ]
+    const objects = []
+    const spread = 15
+
+    function addObject(x, y, obj) {
+        obj.position.x = x * spread
+        obj.position.y = y * spread
+
+        scene.add(obj)
+        objects.push(obj)
+    }
+
+    function createMaterial() {
+        const material = new THREE.MeshPhongMaterial({
+            side: THREE.DoubleSide
+        })
+
+        const hue = Math.random()
+        const saturation = 1
+        const luminance = .5
+
+        material.color.setHSL(hue, saturation, luminance)
+
+        return material
+    }
+
+    function addSolidGeometry(x, y, geometry) {
+        const mesh = new THREE.Mesh(geometry, createMaterial())
+        addObject(x, y, mesh)
+    }
+
+    function addLineGeometry(x, y, geometry) {
+        const material = new THREE.LineBasicMaterial({ color: 0x000000 })
+        const mesh = new THREE.LineSegments(geometry, material)
+        addObject(x, y, mesh)
+    }
+
+    {
+        const width = 8
+        const height = 8
+        const depth = 8
+        const thresholdAngle = 15
+        addLineGeometry(-1, -2, new THREE.EdgesGeometry(
+                new THREE.BoxGeometry(width, height, depth),
+                thresholdAngle
+            )
+        )
+    }
 
     function resizeRendererToDisplaySize(renderer) {
         const canvas = renderer.domElement
-        const pixelRatio = window.devicePixelRatio
-        const width = canvas.clientWidth * pixelRatio | 0
-        const height = canvas.clientHeight * pixelRatio | 0
+        const width = canvas.clientWidth
+        const height = canvas.clientHeight
         const needResize = canvas.width !== width || canvas.height !== height
 
         if(needResize) {
@@ -68,12 +100,12 @@ function main() {
             camera.updateProjectionMatrix()
         }
 
-        cubes.forEach((cube, index) => {
-            const speed = 1 + index * .1
+        objects.forEach((obj, index) => {
+            const speed = 1 + index * .05
             const rotation = time * speed
 
-            cube.rotation.x = rotation
-            cube.rotation.y = rotation
+            obj.rotation.x = rotation
+            obj.rotation.y = rotation
         })
 
         renderer.render(scene, camera)
